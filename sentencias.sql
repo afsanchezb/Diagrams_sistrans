3e9f9e8c61685d713eec17cb2386a1a1ad1b0f69
afsanchezb <af.sanchezb@uniandes.edu.co>
2021-04-11 22:07:54 -0500

    sql2

---
+++ sentencias.sql
@@ -0,0 +1,298 @@
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Felipe', 'CC', 1007475511, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Juan', 'CE', 0078965783, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Ivan', 'CC', 1001184832, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Andres', 'CC', 1007478990, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Fabian', 'TI', 1001789583, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Alex', 'CC', 4893601857, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Fernando', 'CE', 2503352740, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Duvan', 'CC', 9484224841, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Luis', 'CC', 4127764179, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Elkin', 'TI', 9440889615, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Miguel', 'CC', 9105347044, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Mauricio', 'CE', 2057555402, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Diana', 'CC', 8743502180, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Jessica', 'CC', 5416619509, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Arturo', 'TI', 8866362742, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Johana', 'CC', 3756001865, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Ruth', 'CE', 5743045785, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Laura', 'CC', 6622854599, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Santiago', 'CC', 1767524892, 'Vacunable', 1, 1);
+
+INSERT INTO Ciudadano (nombreciu, tipodocumento, nodocumento, estado, eps, ptoasignadociu)
+VALUES ('Fransisco', 'TI', 9527646332, 'Vacunable', 1, 1);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (1, TO_DATE('17/12/2021', 'DD/MM/YYYY'), 1007475511, 1);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (2, TO_DATE('18/12/2021', 'DD/MM/YYYY'), 0078965783, 2);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (3, TO_DATE('19/12/2021', 'DD/MM/YYYY'), 1001184832, 3);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (4, TO_DATE('20/12/2021', 'DD/MM/YYYY'), 1007478990, 4);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (5, TO_DATE('21/12/2021', 'DD/MM/YYYY'), 1001789583, 5);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (6, TO_DATE('22/12/2021', 'DD/MM/YYYY'), 4893601857, 6);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (7, TO_DATE('23/12/2021', 'DD/MM/YYYY'), 2503352740, 7);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (8, TO_DATE('24/12/2021', 'DD/MM/YYYY'), 9484224841, 8);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (9, TO_DATE('25/12/2021', 'DD/MM/YYYY'), 4127764179, 8);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (10, TO_DATE('26/12/2021', 'DD/MM/YYYY'), 9440889615, 9);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (11, TO_DATE('27/12/2021', 'DD/MM/YYYY'), 9105347044, 10);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (12, TO_DATE('28/12/2021', 'DD/MM/YYYY'), 2057555402, 11);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (13, TO_DATE('29/12/2021', 'DD/MM/YYYY'), 8743502180, 12);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (14, TO_DATE('01/01/2022', 'DD/MM/YYYY'), 5416619509, 13);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (15, TO_DATE('02/01/2022', 'DD/MM/YYYY'), 8866362742, 14);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (16, TO_DATE('03/01/2022', 'DD/MM/YYYY'), 3756001865, 15);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (17, TO_DATE('17/12/2022', 'DD/MM/YYYY'), 5743045785, 16);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (18, TO_DATE('17/12/2022', 'DD/MM/YYYY'), 6622854599, 17);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (19, TO_DATE('17/12/2022', 'DD/MM/YYYY'), 1767524892, 18);
+
+INSERT INTO Cita (id_cita, Fechacit, CiudadanoAsignado, PtoVacAsignado)
+VALUES (20, TO_DATE('17/12/2022', 'DD/MM/YYYY'), 9527646332, 19);
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (1, 'Famisanar', 'Andina', 'xd');
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (2, 'Compensar', 'Caribe', 'xd2');
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (3, 'Colsubsidio', 'Amazonia', 'xd3');
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (4, 'Sura', 'Pacifico', 'xd4');
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (5, 'Suramericana', 'Orinoquia', 'xd5');
+
+INSERT INTO EPSReg (ID_EPSReg, NombreEPS, Region, Direccioneps)
+VALUES (6, 'Salud Total', 'Insular', 'xd6');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (1, 'Hospital', 'cd', 10, 10, 10, 1, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (2, 'Clinica', 'cd2', 10, 10, 10, 3, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (3, 'Centro de salud', 'cd3', 10, 10, 10, 6, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (4, 'Otro', 'cd4', 10, 10, 10, 5, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (5, 'Hospital', 'cd5', 10, 10, 10, 4, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (6, 'Clinica', 'cd6', 10, 10, 10, 2, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (7, 'Centro de salud', 'cd7', 10, 10, 10, 1, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (8, 'Otro', 'cd8', 10, 10, 10, 1, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (9, 'Centro de salud', 'cd9', 10, 10, 10, 3, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (10, 'Clinica', 'cd10', 10, 10, 10, 4, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (11, 'Otro', 'cd11', 10, 10, 10, 6, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (12, 'Hospital', 'cd12', 10, 10, 10, 5, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (13, 'Hospital', 'cd13', 10, 10, 10, 5, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (14, 'Centro de salud', 'cd14', 10, 10, 10, 2, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (15, 'Centro de salud', 'cd15', 10, 10, 10, 4, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (16, 'Hospital', 'cd16', 10, 10, 10, 6, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (17, 'Clinica', 'cd17', 10, 10, 10, 4, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (18, 'Hospital', 'cd18', 10, 10, 10, 1, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (19, 'Hospital', 'cd19', 10, 10, 10, 1, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO PuntoDeVacunacion (id_pto,lugar,direccionpto,maxvacunas,aforo,maxciudadanosdiarios,epsasignada,anotacionespreservacion)
+VALUES (20, 'Otro', 'cd20', 10, 10, 10, 2, 'Espacio de refrigeracion, personal de ciudado');
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (1, 'AstraZeneca', 'Mantegase frio y aislado del sol', 2, 13, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (1, 'CoronaVac', 'Mantegase frio y aislado del sol', 2, 14, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (2, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 15, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (3, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 16, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (4, 'AstraZeneca', 'Mantegase frio y aislado del sol', 2, 17, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (5, 'CoronaVac', 'Mantegase frio y aislado del sol', 2, 18, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (6, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 19, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (7, 'AstraZeneca', 'Mantegase frio y aislado del sol', 2, 20, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (8, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 1, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (9, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 2, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (10, 'AstraZeneca', 'Mantegase frio y aislado del sol', 2, 3, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (11, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 4, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (12, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 5, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (13, 'CoronaVac', 'Mantegase frio y aislado del sol', 2, 6, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (14, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 7, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (15, 'CoronaVac', 'Mantegase frio y aislado del sol', 2, 8, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (16, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 9, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (17, 'AstraZeneca', 'Mantegase frio y aislado del sol', 2, 10, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (18, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 11, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (19, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 12, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+INSERT INTO lotevacuna(id_vac, nombrevac, condicionpreserva, dosisnesesarias, ptoasignadolote, cantidad, fechavac) 
+VALUES (20, 'Pfizer', 'Mantegase frio y aislado del sol', 2, 13, 200, TO_DATE('17/12/2021', 'DD/MM/YYYY'));
+
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (1001184832, 1);
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (8866362742, 3);
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (2057555402, 4);
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (4893601857, 6);
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (5416619509, 5);
+
+INSERT INTO AdminEPSReg (NoDocumento, EPSAsignada)
+VALUES (1767524892, 2);
+
+INSERT INTO AdminPtoVac (NoDocumento, PtoAsignado)
+VALUES (0078965783, 1);
+
+INSERT INTO AdminPtoVac (NoDocumento, PtoAsignado)
+VALUES (5743045785, 10);
+
+INSERT INTO AdminPtoVac (NoDocumento, PtoAsignado)
+VALUES (2503352740, 20);
+
+INSERT INTO AdminPlanVac (NoDocumento, Contrasenia)
+VALUES (1007475511, 'Lolcito123');
+
+INSERT INTO Operador(NoDocumento, PtoVac)
+VALUES (1007478990, 1);
+
+UPDATE Ciudadano
+SET PtoAsignado = "puesto a asignar"
+WHERE Ciudadano.id = "ciudadano a asignar";
